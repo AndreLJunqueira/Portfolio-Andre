8de674a9738576c73624cf78d2f22b9e534deb96
JVrovetta <joao.rovetta@hotmail.com>
2023-12-21 15:11:19 -0300

    Resolved issue with image swapping during initial theme transition

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function dia() {
     const body = document.body
-
-    if(body.classList.contains(`light`)) {
-        body.classList.remove(`light`)
-    } else {
-        body.classList.add(`light`)
-    }
     const img = document.querySelector(`img`)
 
-    if(body.classList.contains(`light`)) {
+    if (body.classList.contains(`light`)) {
+        body.classList.remove(`light`)
         img.setAttribute(`src`, `AndréP.png`)
     } else {
+        body.classList.add(`light`)
         img.setAttribute(`src`, `andrérecorte.png`)
     }
-
-    
 }
-   
